afda1eef83b39413947cdd92bd2f87a198f0e86c
Igor Motyka <igor_motyka@mail.com>
2020-01-16 18:15:12 +0100

    Added missing i18n

--- pages/features.vue
+++ pages/features.vue
@@ -3,47 +3,46 @@
     <article class="features__feature">
       <img class="features__image" src="../assets/images/people.png" />
       <section>
-        <h2 class="features__header">{{$t('features-one-header')}}</h2>
+        <h2 class="features__header">{{ $t("features-one-header") }}</h2>
         <p>
-          {{$t('features-one-desc-1')}}
-          <span class="hightlight">{{$t('ai')}}</span>
-          {{$t('features-one-desc-2')}}
+          {{ $t("features-one-desc-1") }}
+          <span class="hightlight">{{ $t("ai") }}</span>
+          {{ $t("features-one-desc-2") }}
         </p>
       </section>
     </article>
     <article class="features__feature">
       <section>
-        <h2 class="features__header">{{$t('features-two-header')}}</h2>
+        <h2 class="features__header">{{ $t("features-two-header") }}</h2>
         <p>
-          {{$t('features-two-desc')}}
-          <span class="hightlight">{{$t('personality-friendly-name')}}</span>
-          , {{$t('features-two-throught')}}
-          <span
-            class="hightlight"
-          >{{$t('personality-conspiracy-name')}}</span>
-          , {{$t('features-two-racist-intro')}}
-          <span
-            class="hightlight"
-          >{{$t('personality-racist-name')}}</span>
+          {{ $t("features-two-desc") }}
+          <span class="hightlight">{{ $t("personality-friendly-name") }}</span>
+          , {{ $t("features-two-throught") }}
+          <span class="hightlight">{{
+            $t("personality-conspiracy-name")
+          }}</span>
+          , {{ $t("features-two-racist-intro") }}
+          <span class="hightlight">{{ $t("personality-racist-name") }}</span>
         </p>
       </section>
       <div>
-        <img class="features__image" src="../assets/images/avatar-men-1.png" />
-        <img class="features__image" src="../assets/images/avatar-men-2.png" />
-        <img class="features__image" src="../assets/images/avatar-women-1.png" />
+        <img class="features__image" src="../assets/images/heads.png" />
       </div>
     </article>
     <article class="features__feature">
-      <img class="features__image" src="../assets/icons/logo.svg" />
+      <img
+        class="features__image features__image--logo"
+        src="../assets/icons/logo.svg"
+      />
       <section>
-        <h2 class="features__header">{{$t('features-three-header')}}</h2>
+        <h2 class="features__header">{{ $t("features-three-header") }}</h2>
         <p>
-          {{$t('features-three-desc-1')}}
-          <span class="hightlight">{{$t('one-men')}}</span>
+          {{ $t("features-three-desc-1") }}
+          <span class="hightlight">{{ $t("one-men") }}</span>
           .
-          {{$t('features-three-desc-2')}}
-          <router-link to="#contact">{{$t('contact')}}</router-link>
-          {{$t('features-three-desc-3')}}
+          {{ $t("features-three-desc-2") }}
+          <router-link to="#contact">{{ $t("contact") }}</router-link>
+          {{ $t("features-three-desc-3") }}
         </p>
       </section>
     </article>
@@ -71,7 +70,7 @@ export default {};
     display: flex;
     align-items: center;
     justify-content: space-evenly;
-    margin: 50px 0;
+    margin: 96px 0;
 
     @media (max-width: $md) {
       justify-content: center;
@@ -90,6 +89,9 @@ export default {};
   }
 
   &__image {
+    &--logo {
+      transform: scale(2);
+    }
     @media (max-width: $md) {
       display: none;
     }
